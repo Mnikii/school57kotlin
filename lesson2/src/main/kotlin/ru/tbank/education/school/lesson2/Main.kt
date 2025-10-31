51d086fb986086f461e69af2e99dc47d553d463f
Mnikii <lysovniki@gmail.com>
2025-10-31 12:36:09 +0300

    Solved DemoList

--- lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/Main.kt
+++ lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/Main.kt
@@ -3,7 +3,22 @@ package ru.tbank.education.school.lesson2
 fun main() {
     val list = DemoList.createList()
     println("Original list: $list")
-    TODO()
+
+    val sum = DemoList.sumList()
+    println("Sum of all elements: $sum")
+
+    val sumEven = DemoList.sumEvenList()
+    println("Sum of even elements: $sumEven")
+
+    val multiplied = DemoList.multiplyList()
+    println("List multiplied 2: $multiplied")
+
+    val max = DemoList.maxElement()
+    println("Max element: $max")
+
+    val sortedDesc = DemoList.sortDesc()
+    println("List sorted descending: $sortedDesc")
+
 
     val map = DemoMap.createMap()
     println("Original map: $map")
